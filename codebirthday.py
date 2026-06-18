e5b5cfca7c58fe6ec5eccb42703c4c226908cb97
neksandha14 <neksandha14@gmail.com>
2026-06-18 11:18:12 +0530

    Add files via upload

---
+++ codebirthday.py
@@ -0,0 +1,16 @@
+# Birthday-email-sender-A python application that automatically sends personalized birthday wishes via email using SMTP.
+import smtplib
+from email.mime.text import MIMEText
+sender="neksandha14@gmail.com"
+password="enter_yours"
+receiver=input("Enter receiver email:")
+name=input("enter your name:")
+msg=MIMEText(f"🎂HAPPY BIRTHDAY ,{name}❤️and have a great day🫶🏻")
+msg['subject']="❤️congratulations"
+msg['From']=sender
+msg['To']=receiver
+server=smtplib.SMTP_SSL("smtp.gmail.com", 465)
+server.login(sender,password)
+server.send_message(msg)
+server.quit()
+print("email sent successfully!")
